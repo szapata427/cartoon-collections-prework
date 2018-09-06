9c17589843843a091086874e4df19e0c2ce304db
Steven Zapata <szapata427@gmail.com>
2018-09-06 03:15:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  
+  dwarves.each_with_index do |dwarve, idx|
+    puts "#{idx + 1}. #{dwarve}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  array = []
+
+  veggies.map.each_with_index do |word, idx|
+    caps = word[0].upcase + word[1..-1] + "!"
+
+  end
+
+end
+  
+
+
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? do |call| call.length > 4
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
